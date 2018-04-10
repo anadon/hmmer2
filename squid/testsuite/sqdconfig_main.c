7acbd5210996b78a492f37d0ab7eafcc33f942b8
Josh Marshall <jrmarsha@mtu.edu>
2018-04-09 22:53:28 -0400

    To be honest, I'm still not entirely sure of what the purpose of the squid files are beyond some utility functions.  I'm re-adding them for the sake of completeness.

---
+++ squid/testsuite/sqdconfig_main.c
@@ -0,0 +1,33 @@
+/*****************************************************************
+ * HMMER - Biological sequence analysis with profile HMMs
+ * Copyright (C) 1992-2006 HHMI Janelia Farm
+ * All Rights Reserved
+ * 
+ *     This source code is distributed under the terms of the
+ *     GNU General Public License. See the files COPYING and LICENSE
+ *     for details.
+ *****************************************************************/
+
+/* sqdconfig_main.c
+ * SRE, Tue Mar  5 15:58:27 2002 [St. Louis]
+ * 
+ * Small C program designed to print out information on squid's 
+ * compile-time configuration options - testsuite scripts can
+ * call this to determine what optional stuff is compiled in. 
+ * 
+ * CVS $Id: sqdconfig_main.c 729 2002-03-05 23:11:28Z eddy $
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "squid.h"
+
+int main(void)
+{
+#ifdef HAS_64BIT_FILE_OFFSETS
+  printf("%-30s true\n", "HAS_64BIT_FILE_OFFSETS");
+#else
+  printf("%-30s false\n", "HAS_64BIT_FILE_OFFSETS");
+#endif
+}
